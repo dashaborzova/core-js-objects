4b3bbfe8de935b5de994dada75bf0ce596048440
dashaborzova <dashaborzova@yandex.ru>
2024-03-06 14:44:09 +0300

    feat: pass 13 tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -189,8 +189,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const values = Object.values(obj);
+  return new proto.constructor(...values);
 }
 
 /**
@@ -219,8 +221,12 @@ function fromJSON(/* proto, json */) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    const country = a.country.localeCompare(b.country);
+    const city = a.city.localeCompare(b.city);
+    return country || city;
+  });
 }
 
 /**
